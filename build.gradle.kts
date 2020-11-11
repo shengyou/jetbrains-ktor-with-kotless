a4559340b723db3f8cb3e1a54cb926c6623bb099
Shengyou Fan <shengyoufan@gmail.com>
2020-11-11 16:31:27 +0800

    add kotless dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.kotless.plugin.gradle.dsl.Webapp.Route53
+import io.kotless.plugin.gradle.dsl.kotless
 
 val ktor_version: String by project
 val kotlin_version: String by project
@@ -9,6 +11,7 @@ plugins {
     application
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.serialization") version "1.3.72"
+    id("io.kotless") version "0.1.6" apply true
 }
 
 group = "io.kraftsman"
@@ -33,6 +36,7 @@ dependencies {
     implementation("io.ktor:ktor-jackson:$ktor_version")
     implementation("com.github.javafaker:javafaker:1.0.2")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
+    implementation("io.kotless", "ktor-lang", "0.1.6")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
